ccc80d3cb41f74084aed6acdec6f44ff3d53e4de
Sophie Déziel <courrier@sophiedeziel.com>
2015-05-09 20:58:25 -0400

    page model and controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,30 @@
 class PagesController < ApplicationController
+
+  def index
+    @page = Page.find_by_slug('index')
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def destroy
+
+  end
 end
